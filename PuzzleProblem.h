32e7cf7dcd6b3d578aa694430ea88fd166a77073
poke-key <kunalshrivastav345@gmail.com>
2024-10-30 11:13:17 -0700

    added private variables for problem class

---
+++ PuzzleProblem.h
@@ -0,0 +1,14 @@
+#ifndef PUZZLE_PROBLEM_H
+#define PUZZLE_PROBLEM_H
+
+#include "PuzzleState.h"
+#include <cmath>
+
+class PuzzleProblem {
+
+
+    private: 
+        PuzzleStatePtr initialState; //starting config
+        PuzzleStatePtr goalState; //8 puzzle layout
+
+}
